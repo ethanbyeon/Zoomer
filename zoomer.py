28a37b93ad949014a509c3bf14ca49d5381e7812
Ethan Byeon <ethanbyeon@gmail.com>
2020-09-02 21:52:52 -0700

    added sign-in setup

---
+++ zoomer.py
@@ -0,0 +1,12 @@
+import subprocess
+import pyautogui
+import pandas as pd
+import time
+from datetime import datetime
+
+def sign_in(meeting_id, pswd):
+    subprocess.call(['file path'])
+
+meeting_id = input("Please enter the meeting ID: ")
+meeting_pass = input("Please enter the meeting password: ")
+sign_in(meeting_id, meeting_pass)
