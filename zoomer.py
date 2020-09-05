3984e567b419f6da32dc1a5891141e6ccbe9aab1
Ethan Byeon <ethanbyeon@gmail.com>
2020-09-04 22:18:07 -0700

    added breakout method

--- zoomer.py
+++ zoomer.py
@@ -4,14 +4,29 @@ import time
 
 from datetime import datetime
 
-def start():
-    app_pos = pug.locateCenterOnScreen('images/zoomApp.png')
-    print(app_pos)
-    pug.moveTo(app_pos)
+def check_pos(position):
+    if position:
+        pug.moveTo(position)
+        pug.click()
+    else:
+        return False
+
+def new_meeting():
+
+    meeting_pos = pug.locateCenterOnScreen('images/new_meeting_btn.png')
+    check_pos(meeting_pos)
 
 def attendance():
-    participants_loc = pug.locateCenterOnScreen('images/participants_btn.png')
-    pug.moveTo(participants_loc)
-    pug.click()
 
-attendance()
+    participants_pos = pug.locateCenterOnScreen('images/participants_btn.png')
+    check_pos(participants_pos)
+
+def breakout():
+
+    breakout_pos = pug.locateCenterOnScreen('images/breakout_room_btn.png')
+    if check_pos(breakout_pos) == False:
+        more_pos = pug.locateCenterOnScreen('images/more_btn.png')
+        check_pos(more_pos)
+        more_br_option = pug.locateCenterOnScreen('images/more_br_opt.png')
+        check_pos(more_br_option)
+        
