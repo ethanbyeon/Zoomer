0f41bab084b6d230f2ba61d2ce0546011f0d4dd0
Ethan Byeon <ethanbyeon@gmail.com>
2020-09-08 22:54:17 -0700

    adjusted GUI and attendance

--- display.py
+++ display.py
@@ -3,8 +3,8 @@ import pyautogui as pug
 
 from zoomer import Zoomer
 
-HEIGHT = 500
-WIDTH = 500
+BUTTON_HEIGHT = 2
+BUTTON_WIDTH = 20
 
 BOT = Zoomer()
 
@@ -15,20 +15,19 @@ class Display(tk.Frame):
 
         self.grid()
         self.master.title("Zoomer")
-
-        self.main_grid = tk.Frame(
-            self, bg="gray", bd=3, width=WIDTH, height=HEIGHT
-        )
-        self.main_grid.grid()
+        self.master.resizable(width=False, height=False)
+        self.master.geometry('400x400')
 
         self.create_widgets()
         self.mainloop()
 
     def create_widgets(self):
-        new_meeting_btn = tk.Button(self, text="New Meeting", fg="black", command=BOT.new_meeting)
-        attendance_btn = tk.Button(self, text="Attendance", fg="black", command=BOT.attendance)
 
-        new_meeting_btn.grid(row=0, column=0)
+        new_meeting_btn = tk.Button(self, text="New Meeting", height=BUTTON_HEIGHT, width=BUTTON_WIDTH, fg="black", command=BOT.new_meeting)
+        attendance_btn = tk.Button(self, text="Attendance", height=BUTTON_HEIGHT, width=BUTTON_WIDTH, fg="black", command=BOT.attendance)
+
+        new_meeting_btn.grid(row=0, column=0, padx=125, pady=10)
         attendance_btn.grid(row=1, column=0)
+
 
 Display()
--- zoomer.py
+++ zoomer.py
@@ -10,26 +10,35 @@ class Zoomer():
     def check_pos(self, position):
         if position:
             pug.moveTo(position)
-            pug.click()
         else:
             return False
 
     def new_meeting(self):
 
         meeting_pos = pug.locateCenterOnScreen('images/menu/new_meeting_btn.png')
-        self.check_pos(meeting_pos)
+        if self.check_pos(meeting_pos): pug.click()
 
     def attendance(self):
 
         participants_pos = pug.locateCenterOnScreen('images/meeting/participants_btn.png')
-        self.check_pos(participants_pos)
+        waiting_room_label_pos = pug.locateCenterOnScreen('images/attendance/waiting_room_label.png')
+        admit_remove_btn_pos = pug.locateCenterOnScreen('images/attendance/admit_remove_btn.png')
+
+        if self.check_pos(participants_pos): pug.click()
+        if waiting_room_label_pos:
+            pug.moveTo(waiting_room_label_pos)
+            pug.move(0, 33)
+        
+        if admit_remove_btn_pos:
+            pug.moveTo(admit_remove_btn_pos)
+            pug.click()
 
     def breakout(self):
 
         breakout_pos = pug.locateCenterOnScreen('images/breakout/breakout_room_btn.png')
         if self.check_pos(breakout_pos) == False:
             more_pos = pug.locateCenterOnScreen('images/breakout/more_btn.png')
-            self.check_pos(more_pos)
+            if self.check_pos(more_pos): pug.click()
             more_br_option = pug.locateCenterOnScreen('images/breakout/more_br_opt.png')
-            self.check_pos(more_br_option)
+            if self.check_pos(more_br_option): pug.click()
         
