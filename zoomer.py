a549513eaeebe7efa931707173fc4c48d980e445
Ethan Byeon <ethanbyeon@gmail.com>
2020-09-13 20:42:43 -0700

    organized imgs to dict, changed img coord function

--- zoomer.py
+++ zoomer.py
@@ -1,44 +1,59 @@
 import pyautogui as pug
+import os
 
 from datetime import datetime
+
+images = {
+
+    # USER SCREEN
+    'desktop_window' : 'desktop_window.png',
+
+    # BREAKOUT ROOM
+    'br_btn' : 'breakout_room_btn.png',
+    'more_br_opt' : 'more_br_opt.png',
+    'more_btn' : 'more_btn.png',
+
+    # MEETING
+    'admit_remove_btn' : 'admit_remove_btn.png',
+    'in_the_meeting_label' : 'in_the_meeting_label',
+    'participants_btn' : 'participants_btn.png',
+    'participants_label' : 'participants_label.png',
+    'waiting_room_label' : 'waiting_room_label.png',
+
+    # MENU
+    'join_btn' : 'join_btn.png',
+    'new_meeting_btn' : 'new_meeting_btn.png',
+    'schedule_btn' : 'schedule_btn.png',
+}
 
 class Zoomer():
 
     def __init__(self):
         self.name = 'Zoomer'
 
-    def check_pos(self, position):
-        if position:
-            pug.moveTo(position)
+    def find_img_coordinates(self, img_name, img_folder, scale=''):
+        needle = os.path.abspath('images/' + 'img_folder/' + img_name)
+        haystack = os.path.abspath('images/user/' + images['desktop_window'])
+        
+        img_coordinates = pug.locate(needle, haystack, grayscale=False)
+        if img_coordinates is not None:
+            img_getX, img_getY = pug.center(img_coordinates)
+            return img_getX, img_getY
         else:
-            return False
+            print('Image "' + img_name + '" not found.')
+            return None
 
     def new_meeting(self):
-
-        meeting_pos = pug.locateCenterOnScreen('images/menu/new_meeting_btn.png')
-        if self.check_pos(meeting_pos): pug.click()
+        new_meeting_btn = self.find_img_coordinates(images['new_meeting_btn'], 'menu')
+        pug.click(new_meeting_btn)
 
     def attendance(self):
-
-        participants_pos = pug.locateCenterOnScreen('images/meeting/participants_btn.png')
-        waiting_room_label_pos = pug.locateCenterOnScreen('images/attendance/waiting_room_label.png')
-        admit_remove_btn_pos = pug.locateCenterOnScreen('images/attendance/admit_remove_btn.png')
-
-        if self.check_pos(participants_pos): pug.click()
-        if waiting_room_label_pos:
-            pug.moveTo(waiting_room_label_pos)
-            pug.move(0, 33)
+        participants_btn = self.find_img_coordinates(images['participants_btn'], 'meeting')
+        pug.click(participants_btn)
         
-        if admit_remove_btn_pos:
-            pug.moveTo(admit_remove_btn_pos)
-            pug.click()
-
-    def breakout(self):
-
-        breakout_pos = pug.locateCenterOnScreen('images/breakout/breakout_room_btn.png')
-        if self.check_pos(breakout_pos) == False:
-            more_pos = pug.locateCenterOnScreen('images/breakout/more_btn.png')
-            if self.check_pos(more_pos): pug.click()
-            more_br_option = pug.locateCenterOnScreen('images/breakout/more_br_opt.png')
-            if self.check_pos(more_br_option): pug.click()
+        waiting_room_label = self.find_img_coordinates(images['waiting_room_label'], 'meeting')
+        pug.click(waiting_room_label)
+        pug.move(0,33)
         
+        admit_remove_btn = self.find_img_coordinates(images['admit_remove_btn'], 'meeting')
+        pug.click(admit_remove_btn)
