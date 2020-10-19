e31b04a39729b6efae6aff65b56d12056666f53e
Ethan Byeon <ethanbyeon@gmail.com>
2020-10-18 19:21:51 -0700

    removed tix objects

--- display.py
+++ display.py
@@ -3,11 +3,10 @@ import tkinter as tk
 import zoomer
 
 from tkinter import filedialog
-from tkinter import tix
 
 
 WIN_W = 370
-WIN_H = 235
+WIN_H = 240
 
 BTN_W = 20
 BTN_H = 2
@@ -26,28 +25,18 @@ FONT = ('arial', 10, 'bold')
 
 FILES = {'Input': '', 'Output': ''}
 
-# MESSAGES
-f_in_msg = ("Select an Excel file (.csv) that contains"
-                "\na student roster for the desired class/period.")
-f_out_msg = ("Select an empty Excel file (.csv) that"
-                "\nwould store the attendance results"
-                "\nfor the desired class/period.")
-student_msg = ("Admit students from the waiting room."
-                "\nIf the waiting room is not empty,"
-                "\npress this button again.")
-leaders_msg = ("Admit group leaders from the waiting room."
-                "\nIf the waiting room is not empty,"
-                "\npress this button again.")
-output_msg = ("Please make sure to: "
+# MESSAGE
+tip_msg = ("Please make sure to: "
                 "\nMinimize all unused windows "
-                "\nKeep the Zoom app visible on the desktop"
-                "\n(Hover over a button for more information)")
+                "\nKeep the Zoom app visible on the desktop")
+important_msg = ("\nIf the waiting room is not empty,"
+                    "\npress the desired attendance button again.")
 
 class Display(tk.Frame):
 
     @classmethod
     def main(cls):
-        root = tix.Tk()
+        root = tk.Tk()
         root.title("Zoomer")
         root.iconbitmap('images/app/check.ico')
         root.resizable(width=False, height=False)
@@ -80,36 +69,15 @@ class Display(tk.Frame):
         self.student_btn = tk.Button(self, text="Take Attendance", font=FONT, 
             height=BTN_H, width=BTN_W, borderwidth=0, 
             fg="white", bg=COLOR['mint'], activebackground=COLOR['mango'], activeforeground='white', 
-            command=lambda: attendance("Student", self.f_in_label, self.f_out_label, self.output_label))
+            command=lambda: attendance("Student", self.f_in_label, self.f_out_label))
         self.leaders_btn = tk.Button(self, text="Admit Group Leaders", font=FONT, 
             height=BTN_H, width=BTN_W, borderwidth=0, 
             fg="white", bg=COLOR['mint'], activebackground=COLOR['mango'], activeforeground='white', 
-            command=lambda: attendance("Leader", self.f_in_label, self.f_out_label, self.output_label))
+            command=lambda: attendance("Leader", self.f_in_label, self.f_out_label))
 
         # HELP LABEL
-        self.output_label = tk.Label(self, text=output_msg, font=FONT, bg=COLOR['grey'])
-
-        # BALLOON MESSAGES
-        self.f_in_tip = tix.Balloon(self)
-        self.f_in_tip.config(bg=COLOR['light-grey'])
-        self.f_in_tip.bind_widget(self.f_in_btn, balloonmsg=f_in_msg)
-
-        self.f_out_tip = tix.Balloon(self)
-        self.f_out_tip.config(bg=COLOR['light-grey'])
-        self.f_out_tip.bind_widget(self.f_out_btn, balloonmsg=f_out_msg)
-
-        self.student_tip = tix.Balloon(self)
-        self.student_tip.config(bg=COLOR['light-grey'])
-        self.student_tip.bind_widget(self.student_btn, balloonmsg=student_msg)
-
-        self.leaders_tip = tix.Balloon(self)
-        self.leaders_tip.config(bg=COLOR['light-grey'])
-        self.leaders_tip.bind_widget(self.leaders_btn, balloonmsg=leaders_msg)
-
-        balloons = [self.f_in_tip, self.f_out_tip, self.student_tip, self.leaders_tip]
-        for msg in balloons:
-            for sub in msg.subwidgets_all():
-                sub.config(bg=COLOR['light-grey'])
+        self.tip_label = tk.Label(self, text=tip_msg, font=FONT, bg=COLOR['grey'])
+        self.important_label = tk.Label(self, text=important_msg, font=FONT, bg=COLOR['grey'], fg=COLOR['danger'])
         
         # GRID
         self.f_in_label.grid(row=1, column=0, padx=10, pady=(20,0), sticky='WENS')
@@ -118,10 +86,11 @@ class Display(tk.Frame):
         self.f_in_btn.grid(row=2, column=0, padx=10, pady=(0,10))
         self.f_out_btn.grid(row=2, column=1, padx=10, pady=(0,10))
         
-        self.student_btn.grid(row=3, column=0, pady=(0,5))
-        self.leaders_btn.grid(row=3, column=1, pady=(0,5))
+        self.student_btn.grid(row=3, column=0)
+        self.leaders_btn.grid(row=3, column=1)
 
-        self.output_label.grid(row=4, column=0, pady=15, columnspan=2)
+        self.tip_label.grid(row=5, column=0, pady=0, columnspan=2)
+        self.important_label.grid(row=4, column=0, pady=0, columnspan=2)
         
         # BINDS
         self.student_btn.bind('<Enter>', self.student_hover)
@@ -160,7 +129,7 @@ def addFile(file, label, in_btn, out_btn):
         zoomer.setup_df(FILES['Input'], FILES['Output'])
 
 
-def attendance(student_type, f_in_label, f_out_label, output_label):
+def attendance(student_type, f_in_label, f_out_label):
     if FILES['Input'] != '' and FILES['Output'] != '':
         zoomer.attendance(FILES['Input'], FILES['Output'], student_type)
 
