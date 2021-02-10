2b3c34bafb61880d35e23a725518ad76bf25cc59
Ethan Byeon <ethanbyeon@gmail.com>
2021-02-09 22:02:56 -0800

    removed docstrings & fixed button changes

--- automation.py
+++ automation.py
@@ -6,20 +6,6 @@ import time
 from tkinter import filedialog
 
 def setup_df(input_file, output_file):
-    """Prepares a default dataframe for an attendance sheet.
-
-        This function reads an input file to create a dataframe for the output file
-        that records the student's ID, name, status, and time of admittance. The student names
-        are arranged alphabetically, and the "Status" and "Time" columns are set to "NA".
-
-        Args:
-            input_file: The student roster.
-            output_file: The attendance sheet.
-
-        Returns:
-            None
-    """
-    
     student_df = pd.read_csv(input_file)
     student_df.fillna('NA', inplace=True)
     student = {'Student ID': [], 'Name': [], 'Status': [], 'Time': [], 'Date': []}
@@ -44,25 +30,6 @@ def setup_df(input_file, output_file):
 
 
 def attendance(input_file, output_file, category):
-    """Checks if the student or leader attendance button is pressed.
-
-    This function checks if the dot/more button can be found on the "Participants" tab
-    to ensure that the user has host or co-host permissions.
-    Then, it passes necessary arguments to the function that was called by 
-    the appropriate button.
-
-    If the "Take Attendance" button is pressed, the check_queue() function is called.
-    If the "Admit Group Leaders" button is pressed, the search_leader() function is called.
-
-    Args:
-        input_file: The student roster.
-        output_file: The attendance sheet.
-        category: The type of student (Student, Leader, etc.).
-
-    Returns:
-        None: If the dot/more button cannot be found on the bottom right of the "Participants" tab.
-    """
-
     dot_btn = capture.find_img_coordinates("dot_btn.png", "meeting")
     
     if dot_btn is not None:
@@ -76,10 +43,8 @@ def attendance(input_file, output_file, category):
 
             if category == "Student":
                 validate_students(search_x, search_y, width, height, search_bar, input_file, output_file)
-
             elif category == "Leader":
                 validate_students(search_x, search_y, width, height, search_bar, input_file, output_file, leader=True)
-
     else:
         return None
 
@@ -89,35 +54,6 @@ wait_x, wait_y, wait_w, wait_h = 0, 0, 0, 0
 leader_prep, student_prep = False, False
 absent_students = set()
 def validate_students(x, y, width, height, search_bar, input_file, output_file, leader=False):
-    """Verifies participant names through a dataframe before admission.
-
-    This function types the name of each group leader, found in the student roster, into 
-    the search bar and captures a precise region.
-    Then, the name from the search result is extracted from the captured region and recorded in a set.
-    The name in the set is then added to a "pool" that serves as a base for performing set operations.
-    
-    The present student is identified if the name extracted from the captured
-    region can be found in the student roster.
-
-    The absent student is identified if the name in the student roster
-    cannot be extracted from the captured region.
-
-    After this process, the present_students set and absent_students set are passed into
-    the record_student() function to be recorded into the dataframe (attendance sheet).
-    
-    Args:
-        x: The x-coordinate of the waiting-room label.
-        y: The y-coordinate of the waiting-room label.
-        width: The width of the captured region.
-        height: The height of the captured region.
-        search_bar: The coordinates of the search bar.
-        input_file: The student roster.
-        output_file: The attendance sheet.
-
-    Return:
-        None
-    """
-    
     global absent_students
     students = set()
 
@@ -129,14 +65,12 @@ def validate_students(x, y, width, height, search_bar, input_file, output_file, 
         
         for r in range(len(in_df.iloc[:, 1])):
             info = (in_df.iloc[r, 1]).replace(',', '').split(' ')
-            name = (info[1] + ' ' + info[0])
-            print("DB NAME: " + name)
+            name = info[1] + ' ' + info[0]
 
             if leader_prep:
                 pos = out_df.loc[out_df['Name'] == name].index[0]
                 if out_df.iat[pos, 2] == "ABSENT":
                     students.add(name.lower())
-                    print("DB ABSENT PREP: " + str(students))
                 else:
                     continue
             else:
@@ -145,14 +79,14 @@ def validate_students(x, y, width, height, search_bar, input_file, output_file, 
         for r in range(0, len(out_df.iloc[:, 1:2])):
             for c in out_df.iloc[r, 1:2]:
                 if student_prep:
-                    print("STUDENT PREP: " + student_prep)
                     if out_df.iat[r, 2] == "ABSENT":
                         students.add(c.lower())
                 else:
-                    print("NA STUDENT SLOT: " + str(out_df.iat[r, 2]))
-                    students.add(c.lower())
+                    if out_df.iat[r, 2] == "PRESENT":
+                        continue
+                    else:
+                        students.add(c.lower())
     
-    #FIX CAPILIZATION FOR PREP = TRUE and fix absent_student sets after pressing leaders btn and reg#
     for name in students:
         print(name)
         pug.click(search_bar)
@@ -162,16 +96,15 @@ def validate_students(x, y, width, height, search_bar, input_file, output_file, 
         
         if meeting_label is not None:
             capture.waiting_ss(x, y, width, height, "meeting")
-            wait_x, wait_y, wait_w, wait_h = x, y, width, height
 
             wait_list = capture.get_text_coordinates("waiting_list.png", "meeting")
             wait_name = set(student['Text'].replace('‘','') for student in wait_list)
             
             present_students = wait_name.intersection(students)
             absent_students = students.difference(wait_name)
-            print("ABSENT STUDENTS: " + str(absent_students))
+            print("SIZE: " + str(len(absent_students)))
             
-            record_student(x, y, present_students, absent_students, wait_list, output_file)
+            record_student(x, y, present_students, absent_students, wait_list, output_file, leader)
             search()
 
 
@@ -182,31 +115,7 @@ def search():
         pug.click(blue_close_btn)
 
 
-def record_student(x, y, present_set, absent_set, wait_list, output_file):
-    """Records the status of the student and calls for student admission if necessary.
-
-    This function iterates over each row in the dataframe (attendance sheet) and marks
-    the student either "PRESENT" or "ABSENT" based on the sets passed in as arguments.
-
-    If the student is in the present set, the student is marked "PRESENT" and the
-    x-y coordinates, student's name, and the coordinates of the extracted text(s) from
-    the captured region are passed into the admit_student() function.
-
-    If the student is already marked "PRESENT", this function will skip the student as
-    they were already accounted for.
-    
-    Args:
-        x: The x-coordinate of the waiting-room label.
-        y: The y-coordinate of the waiting-room label.
-        present_set: The set of present students.
-        absent_set: The set of absent students.
-        wait_list: The list of coordinates of each student's name that are found the waiting room.
-        output_file: The attendance sheet.
-        
-    Return:
-        None
-    """
-
+def record_student(x, y, present_set, absent_set, wait_list, output_file, leader):
     output_df = pd.read_csv(output_file)
     output_df.fillna("NA", inplace=True)
 
@@ -226,33 +135,19 @@ def record_student(x, y, present_set, absent_set, wait_list, output_file):
     
     output_df.to_csv(output_file, index=False)
 
-    global student_prep
-    student_prep = True
+    global student_prep, leader_prep
+    if leader:
+        leader_prep = True
+    else:
+        student_prep = True
 
 
 def admit_student(x, y, student, wait_list):
-    """Admits student after validating with the dataframe.
-    
-    This function iterates over each name in the wait_list list to find the
-    coordinates that correspond to the student's name.
-
-    Args:
-        x: The x-coordinate of the waiting-room label.
-        y: The y-coordinate of the waiting-room label.
-        student: The student's name.
-        wait_list: 
-            The list of coordinates of each student's name that 
-            are extracted from the captured region.
-
-    Return:
-        None
-    """
-
     match = None
     
     for person in wait_list:
         if person['Text'] == student:
-            print("FOUND!!")
+            print("FOUND: " + student)
             match = person
 
     if match is not None:
@@ -260,57 +155,3 @@ def admit_student(x, y, student, wait_list):
         capture.waiting_ss(wait_x, wait_y, wait_w, wait_h, "meeting")
         pug.click(pug.locateOnScreen('images/meeting/admit_btn.png', grayscale=True))
               
-
-#TEST
-def test():
-    setup_df("test/groups.csv", "test/out.csv")
-    attendance_test()
-
-def attendance_test():
-    dot_btn = capture.find_img_coordinates("dot_btn.png", "meeting")
-
-    if dot_btn is not None:
-        waiting_room_label = capture.find_img_coordinates("waiting_room_label.png", "meeting")
-        
-        if waiting_room_label is not None:
-            x, y = waiting_room_label[0] - 15, waiting_room_label[1] + 10
-            x2, y2 = dot_btn[0], dot_btn[1] - 10
-            width = x2 - x
-            height = y2 - y
-
-            width, height = check_nonverbal(x, y, width, height, dot_btn[0])
-            wait_x, wait_y, wait_w, wait_h = x, y, width, height
-            capture.waiting_ss(x, y, width, height, "meeting")
-            val_students(x, y, "test/out.csv")
-
-    else:
-        return None
-
-def check_nonverbal(x, y, w, h, dot_x):
-    nonverbal_btns = capture.find_img_coordinates("nonverbal_btns.png", "meeting")
-    
-    if nonverbal_btns is not None:
-        x2, y2 = dot_x, nonverbal_btns[1] - 20
-        new_width = x2 - x
-        new_height = y2 - y
-
-        return new_width, new_height
-    else:
-        return w, h
-
-def val_students(x, y, output_file="test/out.csv"):
-    wait_list = capture.get_text_coordinates("waiting_list.png", "meeting")
-    wait_names = set(student['Text'] for student in wait_list)
-    df_pool = set()
-
-    output_df = pd.read_csv(output_file)
-    output_df.fillna("NA", inplace=True)
-    for r in range(0, len(output_df.iloc[:, 1:])):
-        for c in output_df.iloc[r, 1:]:
-            if c != "NA":
-                 df_pool.add(c)
-
-    present_students = wait_names.intersection(df_pool)
-    absent_students = df_pool.difference(wait_names)
-
-    record_student(x, y, present_students, absent_students, wait_list, output_file="test/out.csv")
